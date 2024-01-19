4c0d230e9e849c403173f1218dd2a23200403af4
sreekanth.pogula <sreekanthpogula2001@gmail.com>
2024-01-19 17:25:39 +0530

    Initial commit

---
+++ constants/data.js
@@ -0,0 +1,111 @@
+const userData = {
+  githubUsername: "sreekanthpogula",
+  name: "Sreekanth Pogula",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "sreekanthpogula2001@gmail.com",
+  phone: "+91 7995745089",
+  address: "Madhapur, Hyderabad, India.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Founder and Developer",
+      company: "CelibriJoy",
+      year: "2023",
+      companyLink: "https://sreekanthvue.wixsite.com/celebrijoy",
+      desc: "Ignite Your Celebration Beyond Boundaries.",
+    },
+    {
+      title: "Software Developer",
+      company: "SenecaGlobal",
+      year: "2022",
+      companyLink: "https://www.senecaglobal.com/",
+      desc: "Contributed to multiple clients projects and lead development team for the internal- tools of the organization.",
+    },
+    {
+      title: "Internship",
+      company: "SenecaGlobal",
+      year: "2022",
+      companyLink: "https://www.senecaglobal.com/",
+      desc: "Developed a poc for the temporary dental-staffing for the US-Government",
+    },
+    {
+      title: "Graduation",
+      company: "Osmania University, Hyderabad.",
+      year: "2022",
+      companyLink: "https://www.osmania.ac.in/",
+      desc: "Major in Information & Technology with a CGPA of 8.45. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Abhyudaya Patashala, Domakonda.",
+      year: "2015",
+      companyLink: "https://upes.co.in",
+      desc: "Subjects being PCM - I barely survived with 95% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/sreekanthpogula143",
+    twitter: "https://twitter.com/sreekanthpogula",
+    linkedin: "https://linkedin.com/in/sreekanthpogula",
+    github: "https://github.com/sreekanthpogula",
+    facebook: "https://facebook.com/sreekanthpogula",
+    Leetcode: "https://Leetcode.io/sreekanthpogula",
+  },
+};
+
+export default userData;
